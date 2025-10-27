aeef136cb04ed076f9625db06119a40cb921674c
Justin Tan <xJust1n.Tanx@gmail.com>
2025-10-27 15:32:06 +0800

    Edited CareerCenterStaff and User class

--- src/entity/CareerCenterStaff.java
+++ src/entity/CareerCenterStaff.java
@@ -1,4 +1,14 @@
 package entity;
 
 public class CareerCenterStaff extends User {
+    private String staffDepartment;
+
+    CareerCenterStaff(String userID, String name, String passwordHash, String staffDepartment) {
+        super(userID, name, passwordHash);
+        this.staffDepartment = staffDepartment;
+    }
+
+    public String getStaffDepartment() {
+        return staffDepartment;
+    }
 }
