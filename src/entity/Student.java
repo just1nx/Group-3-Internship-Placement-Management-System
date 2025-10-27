b642beaec299a4725ee569ab311ceacfbe23f5d5
ben <phangben03@gmail.com>
2025-10-27 15:35:15 +0800

    Edited LoginInterface, Student, and Applications classes

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -1,4 +1,32 @@
 package entity;
 
 public class Student extends User {
+    private int yearOfStudy;
+    private String major;
+    private int numberOfApplications;
+
+    public Student(String id, String password, int yearOfStudy, String major) {
+        super(id, password);
+        this.yearOfStudy = yearOfStudy;
+        this.major = major;
+        this.numberOfApplications = 0;
+    }
+
+    public void applyApplications() {
+        numberOfApplications++;
+    }
+
+    public void viewApplications() {
+        //Able to view the internship he/she applied for, even after visibility is turned off,
+        // and the application status (“Pending”, Successful”, or “Unsuccessful”)
+    }
+
+    public void acceptApplication() {
+        //accepts an internship offer, withdraws from the rest
+    }
+
+    public void requestWithdrawal() {
+        //requests to withdraw an application
+    }
+
 }
