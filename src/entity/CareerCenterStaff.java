d84c6dbd8d18eef48da53b27aeaa2d8f80939033
Justin Tan <xJust1n.Tanx@gmail.com>
2025-11-05 16:22:40 +0800

    Make CareerCenterStaff constructor public

--- src/entity/CareerCenterStaff.java
+++ src/entity/CareerCenterStaff.java
@@ -3,7 +3,7 @@ package entity;
 public class CareerCenterStaff extends User {
     private String staffDepartment;
 
-    CareerCenterStaff(String userID, String name, String passwordHash, String staffDepartment) {
+    public CareerCenterStaff(String userID, String name, String passwordHash, String staffDepartment) {
         super(userID, name, passwordHash);
         this.staffDepartment = staffDepartment;
     }
